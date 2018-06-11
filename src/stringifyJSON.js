fbf86100061edb5a769b3cd4ce4de2c08438dbbb
samMahani <sam.amirmahani@gmail.com>
2018-06-11 12:38:11 -0700

    worked on JSON.stringify

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,77 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+
+  let result = '';
+
+  if (obj === undefined) {
+  //do nothing
+  }
+  if (typeof obj === 'function') {
+  //do nothing
+  }
+
+
+  if (typeof obj === 'string') {
+    result =  '"' + obj + '"';
+  }
+  if (typeof obj === 'number') {
+    result = obj.toString();
+  }
+  if (typeof obj === 'boolean') {
+    result = '"' + obj + '"';
+  }
+  if (obj === null) {
+    result = 'null';
+  }
+  if (Array.isArray(obj)) {
+    result = '[';
+    obj.forEach(function(element) {
+      result = result + stringifyJSON(element); 
+    }
+    // + ']' //join with comma
+  }
+
+  if (typeof obj === 'object') {
+  }
+
+return result;
+//input: 
+  //string
+  //number (negative, decimals)
+  //booleans
+  //null
+  //arrays
+  //objects
+//output:
+  //all the values in string form
+//special cases rejected:
+  //undefined
+  //functions
+
+/*strategy: check all the values using recursion against 
+all the inputs possibilities
+*/
+
+//psuedocode:
+  //set if cases
+    //if obj is string
+      //add " " to string value
+    //if obj is number
+      //toString the number
+    //if obj is boolean
+      //use typeof to determine boolean
+      //return quoted? bool as string
+    //if obj is null
+      //using deep equality, return "null"
+    //if obj is arrays
+      //initialize string variable to '['
+      //use forEach method
+        //insert elements into var and join with comma
+      //end by joining ']'
+    //if obj is objects
+      //initialize string variable to '{'
+      //for var key in object
+        //join with key + ":" + stringifyJSON(value)
+      //finalize with '}'.  ***.join(', ')***
 };
